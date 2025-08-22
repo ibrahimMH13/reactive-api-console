2557252e71b4ffd87b2649138094e68811f601c6
Ibrahim II Musabeh <i.musabah92@gmail.com>
2025-08-22 20:48:43 +0200

    init forntend config

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string
+    readonly VITE_WS_URL: string
+    readonly VITE_COGNITO_DOMAIN: string
+    readonly VITE_COGNITO_CLIENT_ID: string
+    readonly VITE_COGNITO_REDIRECT_URI: string
+    readonly VITE_COGNITO_LOGOUT_URI: string
+
+}
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
+  
